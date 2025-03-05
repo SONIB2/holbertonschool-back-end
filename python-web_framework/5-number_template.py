fca271645bd2250d0a91deb3719337c93a4b1d81
SONIB2 <soniballa2@gmail.com>
2025-03-05 20:03:32 +0100

    python

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -3,38 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'"""
-    return "HBNB"
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Displays 'C' followed by the value of the text variable"""
-    text = text.replace('_', ' ')  # Replace underscores with spaces
-    return f"C {text}"
-
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    """Displays 'Python' followed by the value of the text variable"""
-    text = text.replace('_', ' ')  # Replace underscores with spaces
-    return f"Python {text}"
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """Displays 'n is a number' only if n is an integer"""
-    return f"{n} is a number"
-
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Displays an HTML page with the number if n is an integer"""
-    return render_template("5-number.html", number=n)
+    return render_template('5-number.html', number=n)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
