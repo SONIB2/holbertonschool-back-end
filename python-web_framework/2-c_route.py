9bd1a5020f685e6f5602bd6f8077dbef24550235
SONIB2 <soniballa2@gmail.com>
2025-03-05 19:46:45 +0100

    python

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """Displays 'C' followed by the value of the text variable with underscores replaced by spaces"""
-    return f"C {text.replace('_', ' ')}"
+    return "C " + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
